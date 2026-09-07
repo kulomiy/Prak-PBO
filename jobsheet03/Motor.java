9aa0d66cf848336fca86133822d719e64b74e4c0
kulomiy <nabilapebmahrani@gmail.com>
2026-09-07 11:31:56 +0700

    commit

---
+++ jobsheet03/Motor.java
@@ -0,0 +1,43 @@
+package jobsheet03;
+
+public class Motor {
+    private String platNomor;
+    private boolean statusMesin;
+    private int kecepatan;
+
+    public void setPlatNomor(String platNomor) {
+        this.platNomor = platNomor;
+    }
+    public boolean getStatusMesin() {
+        return statusMesin;
+    }
+
+    public void setStatusMesin(boolean statusMesin) {
+        this.statusMesin = statusMesin;
+    }
+
+    public int getKecepatan() {
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan) {
+        if (!this.statusMesin && kecepatan > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else if (kecepatan > 100) {
+            System.out.println("Kecepatan maksimal adalah 100!");
+            this.kecepatan = 100;
+        } else if (kecepatan<0) {
+            System.out.println("Kecepatan tidak boleh bernilai negatif!");
+            this.kecepatan = 0; 
+        } else {
+            this.kecepatan = kecepatan;
+        }
+    }
+
+    public void displayInfo() {
+        System.out.println("Plat Nomor: " + platNomor);
+        System.out.println("Status Mesin: " + (this.statusMesin ? "On" : "Off"));
+        System.out.println("Kecepatan: " + kecepatan);
+        System.out.println("===================================");
+    }
+}
---
+++ jobsheet03/UserDemo.java
@@ -0,0 +1,9 @@
+package jobsheet03;
+
+public class UserDemo {
+    public static void main(String[] args) {
+        User user1 = new User("annisa.nadya", "annisa.nadya@gmail.com");
+        user1.displayInfo();
+    }
+}
+
